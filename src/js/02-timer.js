d7aa8e3db5cc7169e97f2d4d4cf00bff8849645d
DimaHavr <109313387+DimaHavr@users.noreply.github.com>
2022-11-24 00:25:56 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,46 +3,58 @@ import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const startBtn = document.querySelector('[data-start]');
-const dataDaysEl = document.querySelector('[data-days]');
-const dataSecondsEl = document.querySelector('[data-seconds]');
 const inputEl = document.querySelector('#datetime-picker');
-
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
+const refs = {
+  dataSecondsEl: document.querySelector('[data-seconds]'),
+  dataMinutesEl: document.querySelector('[data-minutes]'),
+  dataHoursEl: document.querySelector('[data-hours]'),
+  dataDaysEl: document.querySelector('[data-days]'),
 };
+
+startBtn.addEventListener('click', () => {
+  timer.start();
+});
+
 const timer = {
   intervalId: null,
   isActive: false,
+
+  onClose(selectedDates) {
+    const selectedTime = new Date(selectedDates).getTime();
+    const currentTime = Date.now();
+    const result = selectedTime - currentTime;
+    if (currentTime > selectedTime) {
+      return Notify.failure('Please choose a date in the future');
+    }
+    updateTimerFace(convertMs(result));
+  },
 
   start() {
     if (this.isActive) {
       return;
     }
-    console.log('Start');
     this.isActive = true;
 
-    this.intervalId = setInterval(() => {}, 1000);
+    this.intervalId = setInterval(() => {
+      this.onClose();
+    }, 1000);
   },
 };
 
-startBtn.addEventListener('click', () => {
-  timer.start();
-  updateClockFace();
-});
+flatPicker(inputEl, timer);
 
-flatPicker(inputEl, options);
+function updateTimerFace({ days, hours, minutes, seconds }) {
+  refs.dataSecondsEl.textContent = `${seconds}`;
+  refs.dataMinutesEl.textContent = `${minutes}`;
+  refs.dataHoursEl.textContent = `${hours}`;
+  refs.dataDaysEl.textContent = `${days}`;
+}
 
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
 
-function updateClockFace({ days, hours, minutes, seconds }) {
+function updateClockFace() {
   dataDaysEl = `${days}`;
   dataSecondsEl = `${seconds}`;
   console.log(dataSecondsEl);
@@ -68,37 +80,3 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
-
-// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-
-// const bodyEl = document.querySelector('body');
-
-// const timer = {
-//   intervalId: null,
-//   isActive: false,
-
-//   start() {
-//     if (this.isActive) {
-//       return;
-//     }
-
-//     this.isActive = true;
-
-//     this.intervalId = setInterval(() => {}, 1000);
-//   },
-
-//   stop() {
-//     clearInterval(this.intervalId);
-//     this.isActive = false;
-//   },
-// };
-
-// startBtn.addEventListener('click', () => {
-//   timer.start();
-// });
-
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
